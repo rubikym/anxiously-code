c7e6782d90f187aa264b2417f61ab8cd5a024e23
rubikym <rubikym152023@gmail.com>
2018-12-15 01:54:56 +0300

    task 3. Implementation of the solution of the problem of Collatz sequence

---
+++ CollatzProject/CollatzProject/WorkWithConsole.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzProject
+{
+    public class WorkWithConsole
+    {
+        public string outputValue { get; set; }
+        public string inputValue { get; set; }
+
+        public WorkWithConsole()
+        {
+            outputValue = "Введите натуральное число:";
+        }
+
+        public WorkWithConsole(string value)
+        {
+            outputValue = value;
+        }
+
+        public void OutputValue()
+        {
+            Console.WriteLine(outputValue);
+        }
+
+        public void EnterValue()
+        {
+            inputValue = Console.ReadLine();
+        }
+    }
+}
